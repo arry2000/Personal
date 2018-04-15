58b4683b9a197d1bce2620621fb98a37a6115fc5
Arry2000 <vanderstraeten.arne@hotmail.com>
2018-04-15 15:57:37 +0200

    Added the D&D area to personal website ... so now it's part of the bigger project

---
+++ Personal/Areas/DungeonsAndDragons/Controllers/DandDController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Personal.Areas.DungeonsAndDragons.Controllers
+{
+    public class DandDController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult WorldMap()
+        {
+            return View();
+        }
+
+        public PartialViewResult GetMaps(string region)
+        {
+            string partialViewName = "";
+            switch (region)
+            {
+                case "WORLD":
+                    partialViewName = "_World";
+                    break;
+                case "IONA":
+                    partialViewName = "_Iona";
+                    break;
+                case "CIENDOR":
+                    partialViewName = "_Ciendor";
+                    break;
+                case "BRUSLAND MOUNTAINS":
+                    partialViewName = "_BruslandMountains";
+                    break;
+                default:
+                    break;
+            }
+
+            return PartialView(partialViewName);
+        }
+    }
+}
---
+++ Personal/Areas/DungeonsAndDragons/DungeonsAndDragonsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Personal.Areas.DungeonsAndDragons
+{
+    public class DungeonsAndDragonsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DungeonsAndDragons";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DungeonsAndDragons_default",
+                "DungeonsAndDragons/{controller}/{action}/{id}",
+                new { controller = "DandD", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
